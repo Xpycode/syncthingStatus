17f60ab4aa712f0a85edf2cc4f8928c1b3cddccf
Luces Umbrarum <gm.konsortium@yahoo.dk>
2025-08-08 02:26:42 +0200

    Initial Commit

---
+++ syncthingStatus/syncthingStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  syncthingStatusApp.swift
+//  syncthingStatus
+//
+//  Created by sim on 08.08.25.
+//
+
+import SwiftUI
+
+@main
+struct syncthingStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
